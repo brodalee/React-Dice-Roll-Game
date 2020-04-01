c9d62d468f1e67a916c0902e95ca89ebe91f6f11
Brodalee <boubou-28@live.fr>
2020-04-01 23:28:13 +0200

    Initialize project using Create React App

---
+++ Entities/GameResponse.js
@@ -0,0 +1,13 @@
+class GameResponse {
+    constructor(id, winner, score, forscore, turnnumber) {
+        this.id = id;
+        this.winner = winner;
+        this.score = score;
+        this.forscore = forscore;
+        this.turnnumber = turnnumber;
+    }
+
+    static toResponseObject({id, winner, score, forscore, turnnumber}) {
+        return new this(id, winner, score, forscore, turnnumber);
+    }
+}
